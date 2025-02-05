ccf8c451ef03fbea39a6951472ccc080996e5eb4
maferozi <maferozi373@gmail.com>
2025-02-05 18:01:22 +0500

    adding about section

--- js/script.js
+++ js/script.js
@@ -1,1 +1,17 @@
-// Add custom JavaScript here
+function runTypingEffect() {
+  const text = 'I am Asad Ferozi.';
+  const typingElement = document.getElementById('typing-text');
+  const typingDelay = 100;
+
+  typeText(text, typingElement, typingDelay);
+}
+
+function typeText(text, typingElement, delay) {
+  for (let i = 0; i < text.length; i++) {
+    setTimeout(() => {
+      typingElement.textContent += text.charAt(i);
+    }, delay * i);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', runTypingEffect);
